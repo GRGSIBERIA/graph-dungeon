2dccc5c4fa6a11a425ec3eb19785653c0b7c8b6b
grgsiberia <nanashi4129@gmail.com>
2015-08-14 22:27:59 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DungeonMaker
+//
+//  Created by Eiichi Takebuchi on 2015/08/14.
+//  Copyright (c) 2015年 Eiichi Takebuchi. All rights reserved.
+//
+
+#include <iostream>
+
+void main()
+{
+    printf("hello");
+}
