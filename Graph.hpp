884afa7472917dbcdda74e3e14fbb182c251ab67
grgsiberia <nanashi4129@gmail.com>
2015-08-14 22:54:16 +0900

    add Graph.hpp

---
+++ Graph.hpp
@@ -0,0 +1,20 @@
+//
+//  Graph.hpp
+//  DungeonMaker
+//
+//  Created by Eiichi Takebuchi on 2015/08/14.
+//  Copyright (c) 2015年 Eiichi Takebuchi. All rights reserved.
+//
+
+#ifndef DungeonMaker_Graph_hpp
+#define DungeonMaker_Graph_hpp
+
+namespace dungeon
+{
+    class Graph
+    {
+    public:
+    };
+}
+
+#endif
