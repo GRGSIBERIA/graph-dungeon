3b3938bc1538a8cb87693baef5029f549bac535b
grgsiberia <nanashi4129@gmail.com>
2015-08-14 22:32:03 +0900

    add Rect.hpp

---
+++ Rect.hpp
@@ -0,0 +1,21 @@
+//
+//  Rect.hpp
+//  DungeonMaker
+//
+//  Created by Eiichi Takebuchi on 2015/08/14.
+//  Copyright (c) 2015年 Eiichi Takebuchi. All rights reserved.
+//
+
+#ifndef DungeonMaker_Rect_hpp
+#define DungeonMaker_Rect_hpp
+
+namespace dungeon
+{
+    class Rect
+    {
+    public:
+        int x, y, width, height;
+    };
+}
+
+#endif
