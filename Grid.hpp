bcc41ae17c0cd1ab02ced207c8970b77a383bfcf
grgsiberia <nanashi4129@gmail.com>
2015-08-14 22:31:10 +0900

    add Grid.hpp

---
+++ Grid.hpp
@@ -0,0 +1,20 @@
+//
+//  Grid.hpp
+//  DungeonMaker
+//
+//  Created by Eiichi Takebuchi on 2015/08/14.
+//  Copyright (c) 2015年 Eiichi Takebuchi. All rights reserved.
+//
+
+#ifndef DungeonMaker_Grid_hpp
+#define DungeonMaker_Grid_hpp
+
+namespace dungeon
+{
+    class Grid
+    {
+    public:
+    };
+}
+
+#endif
